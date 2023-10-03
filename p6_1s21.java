d33a8dd65049ddd6c2fe90b079b2480c0a6d5a4a
David Moctezuma Pérez <144501989+DavidMoctezum@users.noreply.github.com>
2023-10-03 14:43:24 -0600

    p6_1s21.java

--- p6_1s21.java
+++ p6_1s21.java
@@ -2,7 +2,7 @@ class p6_1s21{
 	public static void main (String[] args){
 	String	nombre;
 	String	edad;
-	nombre = "Cesar";
+	nombre = "David";
 	edad = "18";
 	System.out.println("el valor para nombre es " + nombre);
 	System.out.println("la edad es " + edad);

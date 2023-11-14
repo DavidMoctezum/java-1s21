4650a6057dd3144b8729877f100e0c2c7454cb5d
David Moctezuma Pérez <144501989+DavidMoctezum@users.noreply.github.com>
2023-11-14 15:47:02 -0600

    Add files via upload

---
+++ P3/P3p2_1s21.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class P3p2_1s21{
+	static Scanner teclado = new Scanner(System.in);
+	public static void Suma(){
+		System.out.println("Suma de 2 numeros");
+		System.out.println("Ingrese el 1er numero");
+		int a = teclado.nextInt();
+		System.out.println("Ingrese el 2do numero");
+		int b = teclado.nextInt();
+		int c = a+b;
+		System.out.println("La suma fue " + c);
+	}
+	public static void Resta(){
+		System.out.println("Resta de 2 numeros");
+		System.out.println("Ingrese el 1er numero");
+		int a = teclado.nextInt();
+		System.out.println("Ingrese el 2do numero");
+		int b = teclado.nextInt();
+		int c = a-b;
+		System.out.println("La resta fue " + c);
+	}
+	public static void Division(){
+		System.out.println("Division de numeros");
+		System.out.println("Ingrese sus primeros numeros");
+		int a = teclado.nextInt();
+		System.out.println("Ingrese sus segundos numeros");
+		int b = teclado.nextInt();
+		int c = a/b;
+		System.out.println("La division fue " + c);
+	}
+	public static void Multiplicacion(){
+		System.out.println("Multiplicacion de numeros");
+		System.out.println("Ingrese sus primeros numeros");
+		int a = teclado.nextInt();
+		System.out.println("Ingrese sus segundos numeros");
+		int b = teclado.nextInt();
+		int c = a*b;
+		System.out.println("La multiplicacion fue " + c);
+	}
+	public static void Salir(){
+		System.out.println("Salir de las operaciones logicas");
+	}
+	public static void Menu(){
+		System.out.println("Operaciones Basicas");
+		System.out.println("A) Suma");
+		System.out.println("B) Resta");
+		System.out.println("C) Division");
+		System.out.println("D) Multiplicacion");
+		System.out.println("S) Salir");
+		System.out.println("¿Que opcion deseas?");
+	}
+	public static void main(String[] args) {
+		boolean estado = true;
+		while (estado){
+		Menu();
+		String opc=teclado.nextLine();
+		if (opc.toupper().equelas ("A")) suma();
+		if (opc.equals("B")) resta();
+		if (opc.equals("C")) division();
+		if (opc.equals("D")) multiplicacion();
+		if (opc.equals("S")) salir();
+	}
+}
